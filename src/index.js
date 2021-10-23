893b6cf940af771b8a6c31975005b7fb785a2ef3
halimyusuf <haleemyoosuph@gmail.com>
2021-10-23 22:56:43 +0100

    chore: socket.io config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import app from './app';
 import config from './config';
+import socketio from 'socket.io';
+import execSocket from './socket';
 
 const port = process.env.PORT || config.port;
 
+let server = app;
 if (config.NODE_ENV !== 'test') {
-  app.listen(config.port, console.log(`app running on port ${port}`));
+  server = app.listen(config.port, console.log(`app running on port ${port}`));
 }
+
+const io = socketio(server, {
+  cors: '*'
+});
+execSocket(io);
 
 export default app;
