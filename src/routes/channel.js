57d443a5e8e068d2dfb25bd006a75a570299b1e5
halimyusuf <haleemyoosuph@gmail.com>
2021-11-03 21:31:42 +0100

    fixed typo

--- src/routes/channel.js
+++ src/routes/channel.js
@@ -2,10 +2,10 @@ import { Router } from 'express';
 import asyncFn from '../utils/asyncFn';
 import auth from '../middleware/auth';
 import validator from '../validator/channel';
-import { createchannel, fetchWorkspaceChannels } from '../controllers/channel';
+import { createChannel, fetchWorkspaceChannels } from '../controllers/channel';
 const channelRouter = Router();
 
 channelRouter.get('/:workspace', auth, asyncFn(fetchWorkspaceChannels));
-channelRouter.post('/new', validator, auth, asyncFn(createchannel));
+channelRouter.post('/:workspace/new', validator, auth, asyncFn(createChannel));
 
 export default channelRouter;
