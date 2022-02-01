ce58d52883390894dbd0921fb29c2debb46f90d6
halimyusuf <haleemyoosuph@gmail.com>
2022-02-01 01:27:41 +0100

    chore: add event listeners to each workspace (using namespaces)

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,4 @@
+import Member from './models/member';
 export default function (io) {
   io.use((socket, next) => {
     const user = socket.handshake.auth;
@@ -9,21 +10,43 @@ export default function (io) {
     next();
   });
 
-  io.on('connection', (socket) => {
+  io.on('connection', async (socket) => {
     // emit all users
     const users = [];
+    // console.log(io.of('/').sockets.keys());
     for (let [id, socket] of io.of('/').sockets) {
       users.push({
         userId: socket.id,
         username: socket.username
       });
     }
-    console.log(users);
+    // console.log(users);
     socket.emit('users', users);
     socket.broadcast.emit('user connected', {
-      userId: socket.id,
+      userId: socket.userId,
       username: socket.username
     });
-    // emit all active namespaces
+
+    console.log(socket.userId);
+    const workspaces = await Member.find({ user: socket.userId }).populate('workspace');
+    // console.log(workspaces);
+    // console.log(ioyarn .sockets.sockets);
+    workspaces.forEach((workspace) => {
+      const ws = workspace.workspace;
+      if (ws) {
+        // console.log(`${ws.name}-${ws._id}-${ws.createdBy}`);
+        io.of(`/${ws.name}-${ws._id}-${ws.createdBy}`).on('connection', (wsSocket) => {
+          // console.log(socket);
+          // const wsSocket = nSocket.nsp;
+          // console.log('connected-' + ws.name);
+          wsSocket.emit('WSConn', 'Joined ' + ws.name);
+          wsSocket.once('wsClientConn', () => {
+            console.log('Connect at client');
+          });
+        });
+      }
+    });
+    // on join room
+    // io.of('/');
   });
 }
